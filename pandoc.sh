371d33fc850b88dcf567ed606cbe7c28619fd263
Don Willingham <don.willingham@gmail.com>
2017-04-24 22:48:31 -0400

    Don't convert .full.txt files

--- pandoc.sh
+++ pandoc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 for txt in `find . -name "*.txt"` ; do
-   mdfile=`echo $txt | sed 's/\.txt/\.md/'`
-   pandoc -s -S --from=mediawiki --to=markdown_github $txt --output=$mdfile
+   if [ ! -z "$(echo $txt | sed 's/.*\.full\.txt//')" ] ; then
+      mdfile=`echo $txt | sed 's/\.txt/\.md/'`
+      pandoc -s -S --from=mediawiki --to=markdown_github $txt --output=$mdfile
+   fi
 done
